1f4884aba69a62e7c24b6c2946e1ae67d9648858
nh3223 <nhudders@gmail.com>
2020-03-22 23:17:34 -0700

    Completed Chapter 2 of tutorial

--- flask_tutorial/microblog/app/routes.py
+++ flask_tutorial/microblog/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username': 'Neal'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Seattle!'
+        },
+        {
+            'author': {'username': 'Alice'},
+            'body': 'The Princess Bride is my favorite movie!'
+        }
+    ]
+    return render_template('index.html', title = 'Home Page', user=user, posts=posts)
+
