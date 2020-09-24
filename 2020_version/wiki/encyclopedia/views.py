1a3b8a5956c9b6de278cff85afb9d366119a6747
nh3223 <31948410+nh3223@users.noreply.github.com>
2020-09-24 12:37:41 -0700

    Project_1 complted through Search

--- 2020_version/wiki/encyclopedia/views.py
+++ 2020_version/wiki/encyclopedia/views.py
@@ -1,13 +1,17 @@
 import markdown2
-
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
 from . import util
 
+class SearchForm(forms.Form):
+    query = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Search Encyclopedia',
+        'class': 'search'
+        }))
 
 def index(request):
+    search(request)
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -20,3 +24,17 @@ def entry(request, title):
         'content': markdown2.markdown(util.get_entry(title))
     })    
 
+def search(request):
+    query = request.GET.get('q')
+    if query is not None:
+        if query in util.list_entries():
+            return render(request, 'encyclopedia/entry.html', {
+                    'title': query,
+                    'content': markdown2.markdown(util.get_entry(query))
+                })  
+        entries = [entry for entry in util.list_entries() if query.lower() in entry.lower()]
+        return render(request, 'encyclopedia/search_results.html', {
+                'entries': entries 
+        })
+
+          
