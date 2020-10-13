2e476f42d106d1b9d2f13bee20b4b9ff118c8ff1
nh3223 <31948410+nh3223@users.noreply.github.com>
2020-10-13 11:24:24 -0700

    Added most view email content functionality and added additional inbox view functionality

--- 2020_version/mail/mail/static/mail/inbox.js
+++ 2020_version/mail/mail/static/mail/inbox.js
@@ -14,6 +14,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -42,6 +43,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#read-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
@@ -51,18 +53,66 @@ function load_mailbox(mailbox) {
   fetch('/emails/' + mailbox)
   .then(response => response.json())
   .then(emails => {
-    emails.forEach(email => {
-      let email_details = '<div class="email-detail '
-      if (email['read'] == true) {
-        email_details += 'style="background-color: gray '
-      }
-      email_details += `>From: ${email['sender']}<span class="tab">`
-      email_details += `Subject: ${email['subject']}</span><span class="tab">`
-      email_details += `Time: ${email['timestamp']}</span></div>`
-      document.querySelector('#emails-view').innerHTML += email_details
-    })
+    let email_details = format_inbox(emails)
+    console.log(email_details)
+    document.querySelector('#emails-view').innerHTML += email_details
   });
-   
+}
 
-  //document.querySelector('#emails-view').append(element);
+function load_email(email_id) {
+  
+  // Show the selected email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-email-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Retrieve and show the selected email
+  fetch('/emails/' + email_id)
+  .then(response => response.json())
+  .then(email => {
+    let email_content = format_email_content(email)
+    document.querySelector('#read-email-view').innerHTML = email_content
+  });
+
+  // Mark email as read
+  fetch('/emails/' + email_id, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+}
+
+function format_inbox(emails) {
+  let email_details = ''
+  emails.forEach(email => {
+    email_details += `<div class="email-detail" onclick="load_email(${email['id']})" style="cursor:pointer;` 
+    if (email['read'] == true) {
+      console.log('TRUE')
+      email_details += 'background-color: gray;'
+    }
+    email_details += '">'
+    email_details += `From: ${email['sender']}<span class="tab">`
+    email_details += `Subject: ${email['subject']}</span><span class="tab">`
+    email_details += `Time: ${email['timestamp']}</span></div>`
+  });
+  return email_details;
+}
+
+function format_email_content(email) {
+  // Show general email content
+  let email_content = '<div>'
+  email_content += `From: ${email['sender']}<hr>`
+  email_content += `To: ${email['recipients']}<hr>`
+  email_content += `Time: ${email['timestamp']}<hr>`
+  email_content += `Subject: ${email['subject']}<hr>`
+  email_content += `Body: ${email['body']}</div>`
+
+  // Add archive button
+  let button_value = 'Archive'
+  if (email['archived'] == true) {
+    button_value = 'Remove from Archive'
+  }
+  email_content += `<button class="btn btn-sm btn-outline-primary" id="archive">${button_value}</button>`
+  return email_content
 }
